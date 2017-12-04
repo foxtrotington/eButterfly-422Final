ffb772d749f0e5726bc49b79b5dffa5df074a10b
jetjr <jamesthornton@email.arizona.edu>
2017-12-04 08:50:31 -0700

    clear pbs settings

---
+++ scripts/get_sdm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#PBS -W group_list=
+#PBS -q standard
+#PBS -l select=1:ncpus=4:mem=8gb
+#PBS -l pvmem=7gb
+#PBS -l walltime=48:00:00
+#PBS -l cput=48:00:00
+#PBS -M netid@email.arizona.edu
+#PBS -m bea
+
+#LOAD R
+module load R
+
+#MOVE INTO DIRECTORY CONTAINING OBSERVATIONS
+cd $FILE_DIR
+
+#STORE ALL OBSERVATIONS FILES INTO LIST
+ls *.csv > list
+
+#FOR OBSERVATION FILE IN LIST, RUN SDM TO GENERATE MAPS
+for file in $(cat list); do
+ 
+  ID=$(basename $file | cut -d '-' -f 1)
+  mkdir $OUT_DIR/$ID  
+
+  $SCRIPT_DIR/run-sdm-algo.R $file $ID $OUT_DIR/$ID 
+
+done
+
+
