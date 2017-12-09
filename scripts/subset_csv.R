4009a8b66c90188546788128925eb3abd0b29b59
jetjr <jamesthornton@email.arizona.edu>
2017-12-09 07:46:11 -0700

    correct path

--- scripts/subset_csv.R
+++ scripts/subset_csv.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
-df <- read.csv("ebutterfly_sdm_data_clean.csv")
+df <- read.csv("~/eButterfly-422Final/data/ebutterfly_sdm_data_updated.csv")
 
 for (i in unique(df$species_id)) {
     df <- df[which(df$species_id == i),]
     write.csv(df, file = paste0(i,"-ebutterfly.csv"))
-    df <- read.csv("ebutterfly_sdm_data_clean.csv")
+    df <- read.csv("~/eButterfly-422Final/data/ebutterfly_sdm_data_clean.csv")
 }
